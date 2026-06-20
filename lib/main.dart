3d6f0de5f34c7e7058c0b3454c920f822f450a88
Evand <alfeushendrievan@gmail.com>
2026-06-20 18:29:10 +0700

    revisi main dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,32 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:provider/provider.dart';
+
+import 'firebase_options.dart';
+
 import 'data/providers/cart_provider.dart';
-import './features/auth/screens/splash_screen.dart';
+import 'features/auth/screens/splash_screen.dart';
 
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  print("Background Message: ${message.notification?.title}");
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  debugPrint("Background Message: ${message.notification?.title}");
 }
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-    options: FirebaseOptions(
-      apiKey: "AIzaSyBUK2ge8jlP6jOqrPcMFrJebSv_oBDL5A0",
-      authDomain: "evan-machine-store.firebaseapp.com",
-      projectId: "evan-machine-store",
-      storageBucket: "evan-machine-store.firebasestorage.app",
-      messagingSenderId: "546996661047",
-      appId: "1:546996661047:web:83807e710a1b8c193848b2",
-    ),
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
-  runApp(ChangeNotifierProvider(create: (_) => CartProvider(), child: MyApp()));
+  runApp(
+    ChangeNotifierProvider(create: (_) => CartProvider(), child: const MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(

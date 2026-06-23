37c521519d666d3583ca35cd47be97e028384478
Evand <alfeushendrievan@gmail.com>
2026-06-23 21:31:22 +0700

    hapus import yang ga perlu

--- lib/presentation/cart/screens/checkout_screen.dart
+++ lib/presentation/cart/screens/checkout_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../../data/providers/cart_provider.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import '../../../data/services/transaction_service.dart';
 import '../../../features/auth/screens/otp_authenticator.dart';
 
